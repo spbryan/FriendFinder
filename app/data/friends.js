96efacb596f85031faa8414ab5e90b3d819fb44d
spbryan <seanbryan@comcast.com>
2019-06-15 15:58:35 -0400

    set up basic freind array and simple json response from server

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,3 +3,41 @@
  * Developer: Sean Bryan
  * Date: 2019-06-15
  ***********************************************/
+var friends = [
+    {
+        name: "Bernie F. Optiz III",
+        photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        scores: [
+            3,
+            3,
+            4,
+            2,
+            2,
+            1,
+            3,
+            2,
+            2,
+            3
+        ]
+    },
+    {
+    name: "Michael Deveno",
+        photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        scores: [
+            4,
+            3,
+            1,
+            5,
+            5,
+            1,
+            3,
+            1,
+            2,
+            3
+        ]
+    }
+];
+
+module.exports = {
+    friends : friends
+}
--- server.js
+++ server.js
@@ -3,3 +3,28 @@
  * Developer: Sean Bryan
  * Date: 2019-06-15
  ***********************************************/
+//Dependencies
+var express = require("express");
+var path = require("path");
+var friends = require("./app/data/friends");
+
+//Set up Express App
+var app = express();
+var PORT = process.env.PORT || 3000;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function (req, res) {
+    res.send("Welcome to the Friend Page");
+});
+
+app.get("/api/friends", function (req, res) {
+    return res.json(friends.friends);
+});
+
+
+// Listener
+// ===========================================================
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
