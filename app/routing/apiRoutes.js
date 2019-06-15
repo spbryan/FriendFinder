9bf3653f26f0a2b801f735d12bcda5e72b8461bf
spbryan <seanbryan@comcast.com>
2019-06-15 16:18:33 -0400

    move api routing logic to separate js file to keep code clean and easy to follow

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,3 +3,14 @@
  * Developer: Sean Bryan
  * Date: 2019-06-15
  ***********************************************/
+var friend = require('../data/friends');
+
+module.exports = function (app) {
+    app.get("/", function (req, res) {
+        res.send("Welcome to the Friend Page");
+    });
+
+    app.get("/api/friends", function (req, res) {
+        return res.json(friend.friends);
+    });
+};
--- server.js
+++ server.js
@@ -6,22 +6,16 @@
 //Dependencies
 var express = require("express");
 var path = require("path");
-var friends = require("./app/data/friends");
+var router = require("./app/routing/apiRoutes");
 
 //Set up Express App
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", function (req, res) {
-    res.send("Welcome to the Friend Page");
-});
-
-app.get("/api/friends", function (req, res) {
-    return res.json(friends.friends);
-});
-
+//ROUTER
+require('./app/routing/apiRoutes')(app); 
 
 // Listener
 // ===========================================================
