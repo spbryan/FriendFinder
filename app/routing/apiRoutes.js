39128916bf3a74639c67f3978e4585f32dba4213
spbryan <seanbryan@comcast.com>
2019-06-19 18:27:07 -0400

    add post mechanism for survey results

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,6 +4,7 @@
  * Date: 2019-06-15
  ***********************************************/
 var friend = require('../data/friends');
+var path = require("path");
 
 module.exports = function (app) {
     // app.get("/", function (req, res) {
@@ -13,4 +14,22 @@ module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         return res.json(friend.friends);
     });
+
+    app.post("/api/friends", function (req, res) {
+        var newFriend = req.body;
+        console.log(newFriend);
+        var friendList = friend.friends;
+        var numericScores = newFriend.scores.map(function (x) {
+            return parseInt(x, 10);
+        });
+        newFriend.scores = numericScores;
+        friendList.push(newFriend);
+        res.json(newFriend);
+        // res.redirect(307, path.join(__dirname, "/../public/home.html"));
+    });
+
+    // function convertScores(scores) {
+    //     for(var i = 0; i < scores.length; i++)
+    //     return scores;
+    // }
 };
