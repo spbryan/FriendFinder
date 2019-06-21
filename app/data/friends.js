35f276fb4a4310e4beee87b76c85f2a798f56679
spbryan <seanbryan@comcast.com>
2019-06-21 08:39:23 -0400

    create starting friends to match against

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,8 +5,8 @@
  ***********************************************/
 var friends = [
     {
-        name: "Bernie F. Optiz III",
-        photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        name: "Renato Klay",
+        photo: "https://pm1.narvii.com/6365/970aa842be829972c67e03e0db0f63eeb14098f8_hq.jpg",
         scores: [
             3,
             3,
@@ -21,8 +21,8 @@ var friends = [
         ]
     },
     {
-    name: "Michael Deveno",
-        photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        name: "Eldon Ranell",
+        photo: "https://pbs.twimg.com/profile_images/1054595919059083265/_2uhCnCa_400x400.jpg",
         scores: [
             4,
             3,
@@ -35,9 +35,233 @@ var friends = [
             2,
             3
         ]
+    },
+    {
+        name: "Percival Quito",
+        photo: "https://funnyflix.video/wp-content/uploads/2019/01/bnkIwF4wQZo-370x208.jpg",
+        scores: [
+            5,
+            2,
+            1,
+            3,
+            3,
+            2,
+            4,
+            2,
+            1,
+            5
+        ]
+    },
+    {
+        name: "Daylon Veltry",
+        photo: "https://cdn.buzznick.com/wp-content/uploads/2015/06/selfie-3.jpeg",
+        scores: [
+            2,
+            2,
+            3,
+            2,
+            2,
+            5,
+            1,
+            3,
+            3,
+            3
+        ]
+    },
+    {
+        name: "Pax Granger",
+        photo: "https://i.chzbgr.com/full/9120635904/h7A6211AB/",
+        scores: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            4,
+            3,
+            2,
+            1,
+            3
+        ]
+    },
+    {
+        name: "Kurt Manley",
+        photo: "https://i.imgur.com/dJJUwyh.jpg",
+        scores: [
+            4,
+            5,
+            5,
+            4,
+            5,
+            4,
+            5,
+            4,
+            4,
+            3
+        ]
+    },
+    {
+        name: "Tory Lakin",
+        photo: "http://i.imgur.com/geSpYq1.jpg",
+        scores: [
+            1,
+            2,
+            1,
+            4,
+            2,
+            5,
+            1,
+            2,
+            2,
+            3
+        ]
+    },
+    {
+        name: "Rhoda Bathanie",
+        photo: "http://www.wilsonwrites.com/wp-content/uploads/2014/02/CFC-galley-copy.jpg",
+        scores: [
+            2,
+            3,
+            1,
+            5,
+            2,
+            3,
+            4,
+            2,
+            1,
+            3
+        ]
+    },
+    {
+        name: "Leah Trinity",
+        photo: "http://wroops.com/wp-content/uploads/2017/11/nintchdbpict000369795746.jpg",
+        scores: [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Mariah Hunter",
+        photo: "https://pbs.twimg.com/media/BarZRDQIAAA6wZ8.jpg",
+        scores: [
+            3,
+            1,
+            3,
+            4,
+            5,
+            2,
+            5,
+            3,
+            5,
+            1
+        ]
+    },
+    {
+        name: "Judith Bethel",
+        photo: "https://cdn3-www.thefashionspot.com/assets/uploads/gallery/the-top-12-most-embarrassing-celebrity-selfies/brooklyn_decker_selfie.jpg",
+        scores: [
+            2,
+            4,
+            1,
+            1,
+            2,
+            5,
+            5,
+            5,
+            2,
+            1
+        ]
+    },
+    {
+        name: "Rebecca Shaw",
+        photo: "http://www2.pictures.livingly.com/mp/FpbxHsEocy7x.jpg",
+        scores: [
+            5,
+            1,
+            1,
+            4,
+            2,
+            2,
+            2,
+            4,
+            5,
+            1
+        ]
+    },
+    {
+        name: "Pearl Hester",
+        photo: "https://pbs.twimg.com/media/BhwRDBJCcAA1a3-.jpg",
+        scores: [
+            1,
+            1,
+            1,
+            5,
+            1,
+            2,
+            3,
+            2,
+            5,
+            1
+        ]
+    },
+    {
+        name: "Ruby Miles",
+        photo: "https://vignette.wikia.nocookie.net/8bitbookclub/images/a/a7/Emily.jpg/revision/latest?cb=20170904120313",
+        scores: [
+            3,
+            1,
+            4,
+            2,
+            1,
+            5,
+            3,
+            5,
+            5,
+            4
+        ]
+    },
+    {
+        name: "Julie Selfie",
+        photo: "https://cdn.quizai.com/793e10bd-8a33-451e-b9c4-711a0c51245e/bf5d6a49-b027-4c3e-bb61-0001eae9524a_750x469.png",
+        scores: [
+            2,
+            5,
+            1,
+            1,
+            1,
+            4,
+            4,
+            4,
+            4,
+            4
+        ]
+    },
+    {
+        name: "Amber Waves",
+        photo: "http://pbs.twimg.com/tweet_video_thumb/C6Lv8MNWgAYj8FM.jpg",
+        scores: [
+            5,
+            4,
+            5,
+            3,
+            5,
+            2,
+            5,
+            1,
+            5,
+            4
+        ]
     }
 ];
 
 module.exports = {
-    friends : friends
+    friends: friends
 }
